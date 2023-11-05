0108e0460721d4e6e3ab9407a0d848a449300966
TJmolina <tobias12manchitas@gmail.com>
2023-11-05 20:47:46 -0300

    prueba

--- src/app/api/webPage/[...url]/route.js
+++ src/app/api/webPage/[...url]/route.js
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
 import * as cheerio from 'cheerio';
 import request from "request-promise";
-export const GET = async (req,{params}) => {
+export const GET = async (req, { params }) => {
     try {
-        const url = params.url.join('/').replace(/https?:\/\/?/,'https://');
+        const url = params.url.join('/').replace(/https?:\/\/?/, 'https://');
         const $ = await request({
             uri: url,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+                'credentials': 'include',
+                'Origin': 'https://talkmy.vercel.app/'
             },
             transform: body => cheerio.load(body)
         });
@@ -15,7 +17,7 @@ export const GET = async (req,{params}) => {
         $('body *').removeAttr('class').removeAttr('style').removeAttr('href');
         let html = $('body').html();
         return NextResponse.json(html);
-        
+
     } catch (error) {
         return NextResponse.json(error);
     }
