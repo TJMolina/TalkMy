5944719b7c1acfde6af3977b4f6bb9e9df5a2977
TJmolina <tobias12manchitas@gmail.com>
2023-11-05 20:40:05 -0300

    error get

--- src/app/api/webPage/[...url]/route.js
+++ src/app/api/webPage/[...url]/route.js
@@ -2,16 +2,21 @@ import { NextResponse } from "next/server";
 import * as cheerio from 'cheerio';
 import request from "request-promise";
 export const GET = async (req,{params}) => {
-    const url = params.url.join('/').replace(/https?:\/\/?/,'https://');
-    const $ = await request({
-        uri: url,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
-        },
-        transform: body => cheerio.load(body)
-    });
-    $('style, script, iframe, nav, header, footer, aside, button, select, dialog, noscript, svg, input').remove();
-    $('body *').removeAttr('class').removeAttr('style').removeAttr('href');
-    let html = $('body').html();
-    return NextResponse.json(html);
+    try {
+        const url = params.url.join('/').replace(/https?:\/\/?/,'https://');
+        const $ = await request({
+            uri: url,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
+            },
+            transform: body => cheerio.load(body)
+        });
+        $('style, script, iframe, nav, header, footer, aside, button, select, dialog, noscript, svg, input').remove();
+        $('body *').removeAttr('class').removeAttr('style').removeAttr('href');
+        let html = $('body').html();
+        return NextResponse.json(html);
+        
+    } catch (error) {
+        return NextResponse.json(error);
+    }
 }
