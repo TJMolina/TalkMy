d3c586f4baba5dc99ba90a833728b677e553d2bf
TJmolina <tobias12manchitas@gmail.com>
2023-11-05 21:17:46 -0300

    v3

--- src/app/api/webPage/[...url]/route.js
+++ src/app/api/webPage/[...url]/route.js
@@ -6,6 +6,7 @@ export const GET = async (req, { params }) => {
         const url = params.url.join('/').replace(/https?:\/\/?/, 'https://');
         const $ = await request({
             uri: url,
+            "resolveWithFullResponse": true,
             // headers: {
             //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
             // },
