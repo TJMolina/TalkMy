bc918848a22ac96fdf82e0179e3e5b245b5ec3fa
TJmolina <tobias12manchitas@gmail.com>
2023-11-06 23:24:30 -0300

    pwa offline v2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  })
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-  }
-  
-  module.exports = withPWA(nextConfig)
-  
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
+
+module.exports = withPWA(nextConfig);
