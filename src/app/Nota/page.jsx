2234c3396ecc84925b65f3513e2525420b1286cb
TJmolina <tobias12manchitas@gmail.com>
2023-11-16 22:29:54 -0300

    leer de corrido v1

--- src/app/Nota/page.jsx
+++ src/app/Nota/page.jsx
@@ -13,7 +13,6 @@ import {
   extraerTextoPagina,
   textArea,
 } from "@/services/manejarNotas";
-import { pauseReanudar } from "@/libs/reproductor";
 
 // componentes
 import Header from "@/componentes/header";
@@ -35,67 +34,260 @@ import ApagarLuz from "@/componentes/modoOscuroButton";
 import { useMain } from "../context/mainContext";
 import Link from "next/link";
 
+//------------------------------------------------------------
+
 export default function Leer() {
   const [clikeado, seTclikeado] = useState(null);
   const [isPlay, setIsplay] = useState(false);
   //obtener estas variables y funciones del contexto con useMain
-  const { notas, setNotas, notaEditandoId, estaLogueado } = useMain();
+  const {
+    notas,
+    setNotas,
+    notaEditandoId,
+    setNotaId,
+    estaLogueado,
+    seguirLeyendo,
+  } = useMain();
+
+  //una lista auxiliar ordenada de todas las notas que existen
+  const [notasSiguientes, setNotasSiguientes] = useState(
+    notas.slice().reverse()
+  );
+  //para evitar que cree la misma nota mas de una vez
   let creando = false;
 
+  //------------------------------------------------------------
+
+  //para extraer texto de una pagina
   const extraerTexto = (e) => {
+    //detener evento del formulario
     e.preventDefault();
+    //evio la url a esta funcion que modificara el textarea
     extraerTextoPagina(e.target.urlPagina.value);
+    //cierro la ventana flotante
     cerrarModal();
   };
+
+  //------------------------------------------------------------
+  //altero la lista auxiliar de las notas para lanzar el evento useEffecto correspondiente mas abajo en el codigo. Borro el primer elemento.
+  const leerSiguiente = () => {
+    //array auxiliar que devolvera el mismo array pero sin el primer elemento. Esto porque notasSiguientes es una constante.
+    let aux = notasSiguientes.slice();
+    aux.shift();
+    setNotasSiguientes(aux);
+  };
+
+  //------------------------------------------------------------
 
   const cerrarModal = () =>
     document.getElementById("IrVentanaFlotante").classList.toggle("ver");
 
+  //------------------------------------------------------------
+
   const cambiarIdioma = () =>
     document.querySelector("#google_translate_element > div a")?.click();
 
+  //------------------------------------------------------------
+
   const crearNota = () => {
+    //si el boton esta en play, pausarlo y detener la lectura.
     if (isPlay) pauseReanudar(clikeado, setIsplay, isPlay, seTclikeado);
 
+    //si no está ya en el proceso de crear y el textarea tiene contenido
     if (!creando && textArea().innerText.trim()) {
-      document.querySelector(".parrafoEnfocadoRemarcado")?.classList?.remove("parrafoEnfocadoRemarcado");
+      //desenmarco un parrafo en caso de que siga marcado.
+      document
+        .querySelector(".parrafoEnfocadoRemarcado")
+        ?.classList?.remove("parrafoEnfocadoRemarcado");
 
-      const notaIndividual = textArea().innerHTML.replace(/<\/?font[^>]*>/gi,"");
+      //borro todas las etiquetas font creadas por el traductor de google.
+      const notaIndividual = textArea().innerHTML.replace(
+        /<\/?font[^>]*>/gi,
+        ""
+      );
 
+      //inicio el proceso de crear
       creando = true;
+
+      //una variable auxiliar que almacena todas las notas, esta la voy a modificar y por ello no utilizo las que ya tengo.
       let notas = obtenerNotasLocales();
 
+      //si esta editando una nota, el proceso es otro
       if (notaEditandoId) {
+        //obtengo el indice de esta nota, para editarla
         const index = notas.findIndex((item) => item.id === notaEditandoId);
+
+        //si encontro a la nota
         if (index !== -1) {
+          //edito la nota dentro del array de notas
           notas[index].nota = notaIndividual;
-          if(estaLogueado)subirNotaABD([notaEditandoId, notaIndividual]);
+          //si esta logueado, subo esta nota a la bd
+          if (estaLogueado) subirNotaABD([notaEditandoId, notaIndividual]);
         }
-      } else {
+      }
+      //si no esta editando una nota
+      else {
+        //creo una nota con un id aleatorio y con el atributo nota que contiene todo el texto
         const nota = { id: v4(), nota: notaIndividual };
+        //si ya existen notas en el array de notas, incluir esta al inicio del array, sino agregarla individualmente
         notas = notas ? [nota, ...notas] : [nota];
-        if(estaLogueado)subirNotaABD(nota);
+        //si esta logueado, subo esta nota a la bd
+        if (estaLogueado) subirNotaABD(nota);
       }
 
+      //subo el array de notas al localstorage
       localStorage.setItem("notas", JSON.stringify(notas));
+
+      //actualizo el array principal de notas, este es del contexto principal
       setNotas(notas);
     }
   };
 
+  //------------------------------------------------------------
+  //se ejecuta despues de pausereanudar o se usa directamente cuando la lectura automatica de todas las notas esta encendida
+  const play = async () => {
+    //le paso los elementos hmtl para leer su texto con la sintesis de voz
+    const leerTexto = async (txt) => {
+      return new Promise((resolve) => {
+        const utterance = new SpeechSynthesisUtterance(txt.innerText);
+        //cuando inicia a hablar
+        utterance.onstart = () => {
+          //desenmarco parrafos ya marcados
+          txt.querySelector(".parrafoEnfocadoRemarcado")?.remove();
+          //resalto el elemento
+          txt.classList.add("parrafoEnfocadoRemarcado");
+
+          //remarco el elemento actual como clikeado en caso de que de pausa y reanudar
+          seTclikeado(txt);
+
+          //mantengo enfocado el elemento leido
+          txt.scrollIntoView({ behavior: "smooth", block: "center" });
+        };
+
+        //al finalizar de leer este elemento
+        utterance.onend = () => {
+          //cuando ya leyo todos los elementos
+          if (speechSynthesis.speaking == false) {
+            //desclikeo el ultimo elemento
+            seTclikeado(false);
+            //paso a la siguiente nota
+            leerSiguiente();
+          }
+          //terminar la promesa de este elemento
+          resolve();
+        };
+
+        //hablar
+        speechSynthesis.speak(utterance);
+      });
+    };
+
+    //almacenara todos los parrafos
+    let parrafos;
+
+    //todo el textarea visible
+    let texto = textArea();
+
+    //si clickeo la etiqueta font que genera el traductor de google, lo enfoco en el parrafo principal
+    if (clikeado?.tagName == "FONT") {
+      seTclikeado(clikeado.parentNode.parentNode);
+    }
+    //si no clickeo nada, si clickeo todo el contenedor
+    if (!clikeado || clikeado == texto) {
+      //si escribimos a mano, el primer parrafo nunca tiene etiqueta, asi que lo agrego
+      if (!texto.innerHTML.match(/^<[^>]+>/)) {
+        texto.innerHTML = texto.innerHTML.replace(/^[^<]+/, "<div>$&</div>");
+        texto = textArea();
+      }
+      //devuelvo un array de todos los elementos dentro del contenedor
+      parrafos = Array.from(texto.querySelectorAll("#contenido-archivo > *"));
+
+      //caso contrario, clickeó un parrafo
+    } else {
+      //devuelvo un array de todos los elementos que le siguen al que clikee
+      let elementosSiguientes = [];
+      parrafos = clikeado;
+      while (parrafos) {
+        elementosSiguientes.push(parrafos);
+        parrafos = parrafos.nextElementSibling;
+      }
+      parrafos = elementosSiguientes;
+    }
+
+    //si el array tiene mas de un texto, transformar todos en una promesa
+    parrafos.length > 1
+      ? await Promise.all(parrafos.filter((p) => p.innerText && leerTexto(p)))
+      : //caso contrario, leer ese directamente
+        leerTexto(parrafos[0]);
+  };
+
+  //------------------------------------------------------------
+
+  //ejecutado por el boton de play
+  const pauseReanudar = () => {
+    //si la sintesis no esta hablando y el boton no esta en play, resumen
+    if (speechSynthesis.paused && !isPlay) speechSynthesis.resume();
+    //si la sintesis esta hablando y el boton esta en play, pausa
+    else if (speechSynthesis.speaking && isPlay) speechSynthesis.cancel();
+    //si no esta hablando y el boton esta en play, leer
+    else {
+      //si el textarea esta vacio, no hacer nada
+      if (textArea().innerText.trim().length < 1) return;
+      //caso contrario, leer
+      play();
+    }
+    //cambiar el estado del boton, play o pause
+    setIsplay(!isPlay);
+  };
+
+  //------------------------------------------------------------
+
+  //hacer esto cuando se detecte que quieren leer la siguiente nota
   useEffect(() => {
+    //si la configuracion dice que lea la siguiente nota, si esta en play el boton y si hay notas mas adelante.
+    if (seguirLeyendo && isPlay && notasSiguientes.length > 0) {
+      //especificar que nota se esta leyendo actualmente en caso de que la edite
+      setNotaId(notasSiguientes[0].id);
+      //cambiar el texto del textarea por el de la nota actual
+      textArea().innerHTML = notasSiguientes[0].nota;
+      //darle a leer, es asi para evitar ciertos bugs como el no reconocer el texto del textarea
+      setTimeout(() => {
+        play();
+      }, 20);
+    }
+    //caso contrario, volver a valores predeterminados
+    else {
+      seTclikeado(false);
+      setIsplay(false);
+    }
+  }, [notasSiguientes]);
+
+  //hacer esto al rendereizar la pagina
+  useEffect(() => {
+    //si detecta que esta editando una nota
     if (notaEditandoId) {
-      let texto = notas.find((objeto) => objeto.id === notaEditandoId);
-      textArea().innerHTML = texto.nota;
-    }else{
+      //obtengo el indice de la nota siendo editada dentro del array auxiliar que contiene todas las notas ordenadas
+      const indice = notasSiguientes.findIndex((n) => n.id === notaEditandoId);
+
+      //cambio el texto del textarea por el de la nota siendo editada
+      textArea().innerHTML = notasSiguientes[indice].nota;
+
+      //si el indice no es el primero, recorto todo lo que este detras de la nota editada, para hacer una lista de todas las notas que le sigan
+      if (indice > 0) setNotasSiguientes(notasSiguientes.splice(0, indice - 1));
+
+      //si no esta editando una nota, limpio el textarea del texto conservado de notas anteriores
+    } else {
       textArea().innerHTML = "";
     }
   }, []);
+
+  //------------------------------------------------------------
 
   return (
     <>
       <Header>
         <div className="contenedorLabels">
-          <Link href={'/'} onClick={crearNota} className="label">
+          <Link href={"/"} onClick={crearNota} className="label">
             <AiOutlineSave />
           </Link>
 
@@ -146,7 +338,12 @@ export default function Leer() {
             id="contenido-archivo"
             style={{ fontSize: `${1.5}rem` }}
             className={"contenidoArchivo"}
-            onClick={(e) => !isPlay && seTclikeado(e.target)} //clickea un parrafo especifico
+            onClick={(e) => {
+              console.log("click", isPlay);
+              if (!isPlay) {
+                seTclikeado(e.target);
+              }
+            }} //clickea un parrafo especifico
             translate="yes"
           ></div>
         </div>
@@ -159,14 +356,12 @@ export default function Leer() {
             className={"boton-circular clear-button"}
           />
           <button
-            onClick={() =>
-              pauseReanudar(clikeado, setIsplay, isPlay, seTclikeado)
-            }
+            onClick={pauseReanudar}
             type="button"
             className={"botonPlay boton-circular"}
             id="play"
           >
-            {isPlay ? <AiOutlinePause />: <AiOutlineCaretRight />}
+            {isPlay ? <AiOutlinePause /> : <AiOutlineCaretRight />}
           </button>
         </div>
         <Script src="/pdfLib/pdf.js" />
--- src/app/context/mainContext.jsx
+++ src/app/context/mainContext.jsx
@@ -1,14 +1,22 @@
 "use client";
+//librerias de react
 import { createContext, useContext, useEffect, useState } from "react";
 
+//mis funciones
 import {
   recibirNotasExistentes, //de la bd
   eliminarNotaDeBD,
   obtenerNotasLocales, //del local storage
 } from "@/services/manejarNotas";
 
+//----------------------------------------------------------------------------------
+
+//crear contexto
 const MainContext = createContext();
 
+//----------------------------------------------------------------------------------
+
+//funcion para usar el contexto y sus funciones y variables
 export const useMain = () => {
   const context = useContext(MainContext);
   if (!context)
@@ -16,32 +24,56 @@ export const useMain = () => {
   return context;
 };
 
-export const MainProvider = ({ children }) => {
-  const [estaLogueado, setLogueado] = useState(false);
-  const [notas, setNotas] = useState([]);
-  const [notaEditandoId, setNotaId] = useState("");
-  const [modoOscuro, setModoOscuro] = useState(false);
+//----------------------------------------------------------------------------------
 
-  const luzOnOff = ()=>{
+//funcion principal del contexto
+export const MainProvider = ({ children }) => {
+  //para verificar si esta logueado o no
+  const [estaLogueado, setLogueado] = useState(false);
+  //almacenara todas las notas ya sea locales o de la bd
+  const [notas, setNotas] = useState([]);
+  //almacenara el id si una nota se está editando
+  const [notaEditandoId, setNotaId] = useState("");
+  //interrupto del modo oscuro
+  const [modoOscuro, setModoOscuro] = useState(false);
+  //deberia existir un menu de opciones que me permita modificar esta configuracion. es para determinar si leer todas las notas o solo la seleccionada.
+  const [seguirLeyendo, setSeguirLeyendo] = useState(true);
+  //----------------------------------------------------------------------------------
+  //funcion de interruptor de modo oscuro
+  const luzOnOff = () => {
     document.querySelector("body").classList.toggle("oscuro");
     setModoOscuro(!modoOscuro);
-  }
+  };
 
+  //----------------------------------------------------------------------------------
+  //funcion que en el index te permite eliminar una nota
   const borrarEstaNota = (idBorrar) => {
+    //darle una animacion y estilo al borrar
     document.getElementById(idBorrar)?.classList.add("borrar");
+
+    //despues de darle tienpo a verse a la animacion de borrar, borrar definitivamente la nota
     setTimeout(() => {
+      //borrar la nota de la lista en este contexto
       setNotas(notas.filter((nota) => nota.id !== idBorrar));
+
+      //borrar la nota del almacenamiento, esta funcion la borra tando de la bd como del localstorage
       eliminarNotaDeBD(idBorrar, estaLogueado);
     }, 198);
   };
 
-  
+  //----------------------------------------------------------------------------------
+  //se ejecutara al renderizar el index
   useEffect(() => {
     //Mostrar todas las notas existentes en  la cuenta del usuario al iniciar la pagina
     setNotas(obtenerNotasLocales());
+    //verificare si esta logueado o no
     setLogueado(localStorage.getItem("contraseTalkMyAppUsuario"));
-    if (localStorage.getItem("contraseTalkMyAppUsuario")) recibirNotasExistentes(setNotas, notas);
+    //si esta logueado, recibir notas de la bd
+    if (localStorage.getItem("contraseTalkMyAppUsuario"))
+      recibirNotasExistentes(setNotas, notas);
   }, []);
+
+  //----------------------------------------------------------------------------------
 
   return (
     <MainContext.Provider
@@ -53,7 +85,8 @@ export const MainProvider = ({ children }) => {
         notaEditandoId,
         setNotaId,
         luzOnOff,
-        modoOscuro
+        modoOscuro,
+        seguirLeyendo,
       }}
     >
       {children}
