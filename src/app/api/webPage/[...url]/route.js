9570f18569e6419d6907e923616864ebce39ce73
TJMolina <tobiasmolina12manchitas@gmail.com>
2024-02-15 21:20:17 -0300

    mejora de la api

--- src/app/api/webPage/[...url]/route.js
+++ src/app/api/webPage/[...url]/route.js
@@ -1,41 +1,41 @@
 import { NextResponse } from "next/server";
-import * as cheerio from 'cheerio';
-import request from "request-promise";
-export const GET = async (req,{params}) => {
+import cheerio from 'cheerio';
+import axios from "axios";
+
+export const GET = async (req, { params }) => {
     const url = params.url.join('/').replace(/https?:\/\/?/,'https://');
     try {
-        const $ = await request({
-            uri: url,
+        const response = await axios.get(url, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
-            },
-            transform: body => cheerio.load(body)
+            }
         });
-        $('style, script, iframe, nav, header, footer, aside, button, select, dialog, noscript, svg, input, textarea').remove();
-        $('body *').removeAttr('class').removeAttr('style').removeAttr('href');
-        let html = $('body').html().replace(/\n/g, "");
-        return NextResponse.json(html);
+        const $ = cheerio.load(response.data);
         
+        // Remove unwanted elements and attributes
+        $('style, script, iframe, nav, header, footer, aside, button, select, dialog, noscript, svg, input, textarea').remove();
+        $('[class], [style], [href]').removeAttr('class style href');
+        
+        // Compact HTML and remove unnecessary whitespace
+        let html = $('body').html().replace(/\n/g, "").replace(/<p>&nbsp;<\/p>/g, "");
+        
+        return NextResponse.json(html);
     } catch (error) {
         const op2 = await extraerTextoPagina_op2(url);
-        if(op2){
-            return NextResponse.json(op2)
-        }
-        return NextResponse.json(error)
+        return NextResponse.json(op2 || error);
     }
 }
 
 const extraerTextoPagina_op2 = async (url) => {
     try {
-      const urlBuscar = new FormData();
-      urlBuscar.append("url", url);
-      const respuestaFetchPHP = await fetch(
-        "https://bdtalkmy.000webhostapp.com/AcionNotas/extraerTextoPagina.php",
-        { method: "POST", body: urlBuscar }
-      );
-      const respuestaPHP = await respuestaFetchPHP.text();
-      return respuestaPHP;
-    } catch (e) {
-      return false;
+        const formData = new FormData();
+        formData.append("url", url);
+        const response = await axios.post(
+            "https://bdtalkmy.000webhostapp.com/AcionNotas/extraerTextoPagina.php",
+            formData
+        );
+        return response.data;
+    } catch (error) {
+        return false;
     }
-  };
+};
